e9d87b52689499f57fc9df614fe3693b8df228c6
Julian Pokrovsky <juliankul@gmail.com>
2021-11-18 22:29:02 +0300

    add prop-based testing package

--- test/kanrex_test.exs
+++ test/kanrex_test.exs
@@ -1,8 +1,20 @@
 defmodule KanrexTest do
   use ExUnit.Case
+  use PropCheck
+
   doctest Kanrex
 
   test "greets the world" do
     assert Kanrex.hello() == :world
+  end
+
+  property "always works" do
+    forall type <- term() do
+      boolean(type)
+    end
+  end
+
+  def boolean(_) do
+    true
   end
 end

08eb391b52bf048cafeab7989f25f26ab60d66d9
Julian Pokrovsky <juliankul@gmail.com>
2021-11-21 01:50:33 +0300

    update tests

--- test/kanrex_test.exs
+++ test/kanrex_test.exs
@@ -4,20 +4,58 @@ defmodule KanrexTest do
 
   doctest Kanrex
 
-  # test "greets the world" do
-  #   assert Kanrex.hello() == :world
-  # end
-
-  property "varialbe identifies correctly" do
-    forall v <- var() do
-      Kanrex.var?(v)
+  property "only Kanrex.var is a variable" do
+    forall {v, t} <- {var(), term()} do
+      Kanrex.var?(v) && !Kanrex.var?(t)
     end
   end
 
-  property "variable unification" do
-    forall {var1, var2} <- {var(), var()} do
-      var1 == var2
+  property "walk to the value" do
+    forall {k, v, map} <- map_contains_var() do
+      v == Kanrex.walk(k, map)
     end
+  end
+
+  test "unify two vars" do
+     s = Map.new()
+     u = Kanrex.var(true)
+     v = Kanrex.var(true)
+     assert Kanrex.unify(u,v,s) == s
+  end
+
+  test "unify first var" do
+     s = Map.new()
+     u = Kanrex.var(true)
+     v = true
+     assert Kanrex.unify(u,v,s) == %{u => v}
+  end
+
+  test "unify second var" do
+     s = Map.new()
+     u = true
+     v = Kanrex.var(true)
+     assert Kanrex.unify(u,v,s) == %{v => u}
+  end
+
+  test "unify two lists" do
+     s = Map.new()
+     u = [true, true]
+     v = [Kanrex.var(true), Kanrex.var(true)]
+     assert Kanrex.unify(u,v,s) == %{%Kanrex.Var{id: true} => true}
+  end
+
+  test "unify no match without equality" do
+     s = Map.new()
+     u = true
+     v = false
+     assert Kanrex.unify(u, v, s) == false
+  end
+
+  test "unify no match with equality" do
+     s = Map.new()
+     u = true
+     v = true
+     assert Kanrex.unify(u, v, s) == Map.new
   end
 
   defp var() do
@@ -25,4 +63,23 @@ defmodule KanrexTest do
     sample = :proper_gen.pick(term)
     Kanrex.var(sample)
   end
+
+  defp map_contains_var() do
+    r = Enum.random(2..8)
+    k = Kanrex.var(r-1)
+    v = Kanrex.var(r+1)
+
+    map = 1..r
+    |> Enum.reduce(
+      Map.new(),
+      fn (val, acc) ->
+        Map.put(acc, Kanrex.var(val), Kanrex.var(val+1))
+      end)
+
+    {k, v, map}
+  end
+
+    # defp var_or_term() do
+  #   oneof([var(), term()])
+  # end
 end
